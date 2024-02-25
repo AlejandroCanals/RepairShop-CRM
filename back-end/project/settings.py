fd02889131d7eb6b62b26e4da9e740d8a6523586
iTzSRK <Alejandrocanals17@gmail.com>
2024-02-25 13:35:54 +0100

    debug

--- back-end/project/settings.py
+++ back-end/project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$a1iekb*%#kf5+2*bgho5fj3hytc!)&8-hx*93^g1msnjtct-i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['repairshop-crm.onrender.com']
 
