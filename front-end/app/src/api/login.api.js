07e7602c31d7aefa88ac5d37177bc9d537a8e797
Alejandro Canals <129771462+AlejandroCanals@users.noreply.github.com>
2024-05-03 16:59:26 +0200

    Update login.api.js

--- front-end/app/src/api/login.api.js
+++ front-end/app/src/api/login.api.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
 export const loginAPI = axios.create({
-  baseURL: 'http://127.0.0.1:8000/app/api/auth/login/'
+  baseURL: 'https://repairshop-crm.onrender.com/app/api/auth/login/'
 });
+//  baseURL: 'http://127.0.0.1:8000/app/api/auth/login/'
+
