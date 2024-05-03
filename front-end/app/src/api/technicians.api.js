5e0e7c669b55303d0e005b8fca89878ea999350b
Alejandro Canals <129771462+AlejandroCanals@users.noreply.github.com>
2024-05-03 17:00:52 +0200

    Update technicians.api.js

--- front-end/app/src/api/technicians.api.js
+++ front-end/app/src/api/technicians.api.js
@@ -1,8 +1,12 @@
 import axios from 'axios'
 
 const rmaApi = axios.create({
-    baseURL: 'http://127.0.0.1:8000/app/technicians/'
+    baseURL: 'https://repairshop-crm.onrender.com/app/technicians/'
 });
+    
+//baseURL: 'http://127.0.0.1:8000/app/technicians/'
+
+
 
 export const getListaDeTecnicos = () => {
     return rmaApi.get('/');
